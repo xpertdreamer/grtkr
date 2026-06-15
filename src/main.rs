490e4fc9c7f1543a0f844381481394962be377c4
xpertdreamer <yungnomen@gmail.com>
2026-06-15 23:09:22 +0300

    feat: it was pretty fun

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,16 @@ use std::{fs::File, io::{BufRead, BufReader}};
 
 use clap::Parser;
 
+
+#[allow(non_camel_case_types)]
+#[derive(PartialEq, Debug)]
+enum PAT_KIND
+{
+    DIGIT,
+    DOLLAR,
+    EMPTY
+}
+
 #[derive(Parser)]
 struct CLI {
     // Which pattern look for
@@ -11,23 +21,41 @@ struct CLI {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut count = 0;
     let args = CLI::parse();
+    let pattern = match args.pat.as_str() {
+        "digit" => PAT_KIND::DIGIT,
+        "dollar" => PAT_KIND::DOLLAR,
+        _ => PAT_KIND::EMPTY,
+    };
+
     let f = File::open(&args.file);
     let file = match f {
         Ok(file) => { file },
         Err(error) => { return Err(error.into()); }
     };
+
     let reader = BufReader::new(file);
 
-    // TODO: print some patterns like line-numbers, dollar-signs and etc
     for ln in reader.lines()
     {
         let line = ln?;
         let line = line.trim_end();
-        println!("{} {}", args.pat, line);
+
+        match pattern {
+            PAT_KIND::DIGIT => {
+                println!("{}| {}", count + 1, line);
+            }
+            PAT_KIND::DOLLAR => {
+                println!("$ {}", line);
+            }
+            PAT_KIND::EMPTY => {
+                println!("{}", line);
+            }
+        }
+        count += 1;
     }
 
-    // TODO: file statistics
-    println!("pat: {:?}, file: {:?}", args.pat, args.file);
+    println!("\npattern: {:?}\npath: {:?}\nlines: {:?}", pattern, args.file, count);
     Ok(())
 }
