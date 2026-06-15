9588c9e299b6a8ccef84fa16c0f37bcdedd1eb0d
xpertdreamer <yungnomen@gmail.com>
2026-06-15 20:54:43 +0300

    feat: Argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+use clap::Parser;
 
+#[derive(Parser)]
+struct CLI {
+    // Which pattern look for
+    pat: String,
+    // A path to the file
+    file: std::path::PathBuf
+}
+
+fn main() {
+    let args = CLI::parse();
+    println!("pat: {:?}, file: {:?}", args.pat, args.file);
 }
